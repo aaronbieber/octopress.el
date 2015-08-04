6ece37125c5cf23655b357bea7598deef43778d2
Aaron Bieber <aaron@aaronbieber.com>
2015-08-04 07:49:44 -0400

    Make Commentary match README intro

--- octopress.el
+++ octopress.el
@@ -10,10 +10,17 @@
 
 ;;; Commentary:
 
-;; This package provides a lightweight but fluent wrapper around the
-;; Octopress 3.0 suite of commands used to generate and manage a blog
-;; site. For help using this package, see its README or website as
-;; noted in the header above.
+;; Octopress.el is a lightweight wrapper script to help you interact
+;; with Octopress blog site and the related Jekyll programs. This
+;; package is designed to be unobtrusive and to defer to Octopress and
+;; Jekyll as often as possible.
+
+;; This package was built with the assumption of Octopress 3.0 and
+;; will probably not work with previous (non-gem) versions of
+;; Octopress. Specifically, it expects to be able to use commands like
+;; `octopress new post` rather than the old-style `rake new_post[]`.
+
+;; Full documentation is available as an Info manual.
 
 ;;; Code:
 
